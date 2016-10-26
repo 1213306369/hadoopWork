c8d018d49a632cb4cd127ad2b6bd1435c06fbd7d
mfq <1255312814@qq.com>
2016-10-26 00:20:07 -0700

    streaming2

---
+++ homework/1026/mengfanqi/Streaming.java
@@ -0,0 +1,14 @@
+package mapRed;
+
+import java.util.Scanner;
+
+public class Streaming {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+				while(sc.hasNext()){
+					String line = sc.nextLine();
+					System.out.println(line);
+				}
+	}
+}
