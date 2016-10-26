bf2c7fff14d6e33f9dbcced51352b48026005af9
2908196051 <949195943@qq.com>
2016-10-26 21:56:32 +0800

    Update UrlCount.java

--- homework/1026/zhengkaifa/UrlCount.java
+++ homework/1026/zhengkaifa/UrlCount.java
@@ -6,28 +6,30 @@ public class UrlCount {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		List<String> list = new ArrayList<String>();
 		String ip = null;
 		while (sc.hasNext()) {
 			String line = sc.nextLine();
-			String[] tokens;
-			tokens = line.split("\t");
+			String[] tokens=line.split("\t");
 			String newIp = tokens[0];
-			String values = tokens[1];
+			String url = tokens[1];
+			
 			if (newIp.equals(ip)) {
-					if (!list.contains(values)) {
-						list.add(values);
-					}
-				} else {
-					if(ip!=null)
-						System.out.println(ip+"\t"+list.size());
-						ip=newIp;
-						list.clear();
-						list.add(values);
-				}
+				if(!list.contains(url))
+					list.add(url);
+			}else{
+				if (ip!=null) System.out.println(ip+"\t"+list.size());
+				
+				ip = newIp;
+				list.clear();
+				list.add(url);
+			}
 		}
+		if (ip!=null) System.out.println(ip+"\t"+list.size());
+		else System.out.println("文件是空的");
+		
 		sc.close();
+		
 	}
 
 }
