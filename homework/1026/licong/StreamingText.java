20738ca781d3396bbf105929088e48628430e42b
licong <1538257077@qq.com>
2016-10-26 15:14:08 +0800

    提交

---
+++ homework/1026/licong/StreamingText.java
@@ -0,0 +1,14 @@
+package hehe;
+
+import java.util.Scanner;
+
+public class StreamingText {
+	public static void main(String [] args){
+	Scanner sca=new Scanner(System.in);
+	while(sca.hasNextLine()){
+		String str =sca.nextLine();
+		System.out.println(str);
+		
+	}
+	}
+}
