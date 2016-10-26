704ce4a1bfa29830e8e80717f472f82a689999e9
maziyue <871211390@qq.com>
2016-10-26 22:48:48 +0800

    add a file

---
+++ homework/1026/maziyue/test2.java
@@ -0,0 +1,44 @@
+package streaming;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class test2 {
+	static String ip = null;
+	static String oldIP = null;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		HashSet hashSetIp = new HashSet();
+		HashSet hashSetPath = new HashSet();
+
+		while (sc.hasNext()) {
+
+			String line = sc.nextLine();
+			String[] words = line.split("\t");
+			ip = words[0];
+
+			if (hashSetIp.contains(ip)) {
+
+				hashSetPath.add(words[1]);
+
+			} else {
+
+				System.out.println(oldIP + " " + hashSetPath.size());
+
+				oldIP = ip;
+				hashSetIp.add(ip);
+				hashSetPath.clear();
+				// hashSetPath.add(1);
+				hashSetPath.add(words[1]);
+				hashSetIp.add(words[1]);
+
+			}
+
+		}
+		System.out.println(oldIP + " " + hashSetPath.size());
+
+	}
+
+}
