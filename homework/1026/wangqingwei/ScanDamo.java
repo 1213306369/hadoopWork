d3800164cb24d9f6930528f27ff88106a6788e21
wangqingwei <389579307@qq.com>
2016-10-26 00:03:22 -0700

    scan.java

---
+++ homework/1026/wangqingwei/ScanDamo.java
@@ -0,0 +1,16 @@
+package upload;
+
+import java.util.Scanner;
+
+public class ScanDamo {
+ public static void main(String[] args) {
+	 Scanner scan=new Scanner(System.in);
+		
+		while(scan.hasNext()){
+			String line=scan.next();
+			System.out.println(line);
+			
+		}
+}
+	
+}
