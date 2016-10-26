f09d5e81856cf2ccea6471d140c7c889125225c1
jacoulin <405563383@qq.com>
2016-10-26 15:05:55 +0800

    upload Demo_04.java

---
+++ homework/1026/zhangyoulin/Demo_04.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.Scanner;
+
+public class Demo_04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			System.out.println(sc.nextLine());
+		}
+	}
+}
