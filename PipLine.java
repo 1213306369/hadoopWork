ff03ab5176754c63530aec620934680c61f252fc
pangzhixuan <779379672@qq.com>
2016-10-26 15:13:12 +0800

    PipLine.java

---
+++ PipLine.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class PipLine{
+
+     public static void main(String [] args){
+	 
+	    Scanner sc=new Scanner(System.in);
+		while(sc.hasNext()){
+		    String line=sc.nextLine();
+			System.out.println(line);
+		}
+	}
+}
