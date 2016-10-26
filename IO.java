36114ef4982ed02229481bcfd5591b2340cb65e5
root <root@master.(none)>
2016-10-26 15:11:19 +0800

    提交管道

---
+++ IO.java
@@ -0,0 +1,18 @@
+mport java.util.Scanner;
+
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		while(in.hasNext()){
+			System.out.println(in.next());
+		}
+	}
+
+}
