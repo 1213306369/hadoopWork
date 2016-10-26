5b1967641306886c32670187f36e5d54369a6958
yutila <1357917686@qq.com>
2016-10-26 15:22:13 +0800

    java

---
+++ homework/1026/yulang/runStreaming.java
@@ -0,0 +1,15 @@
+package pratice;
+
+import java.util.Scanner;
+
+public class runStreaming {
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		while(sc.hasNext()){
+			System.out.println(sc.next());
+		}
+
+	}
+
+
+}
