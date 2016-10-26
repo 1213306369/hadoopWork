4865d8404db54a1ca42ea9ce5132e12679402f5f
zhangmingsheng <z1m6s3@163.com>
2016-10-26 15:24:46 +0800

    Éϴ«java

---
+++ homework/1026/zhangmingsheng/Test1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test1 {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	while(scanner.hasNext()){
+		String line = scanner.nextLine();
+		System.out.println(line);
+	}
+}
+}
