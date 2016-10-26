2ec6479ac1412b960542f3a2255f465534136193
gys <762659823@qq.com>
2016-10-26 00:12:28 -0700

    create my dir

---
+++ homework/1026/geyansheng/F.java
@@ -0,0 +1,15 @@
+package friend;
+
+import java.util.Scanner;
+
+public class F {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			String line = sc.next();
+			System.out.println(line);
+			System.out.println("--------------------------");
+		}
+	}
+
+}
