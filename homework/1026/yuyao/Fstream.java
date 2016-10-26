f45aec86d56806f24b1b0d2672e3a81244943e09
yuyaoyao <449270728@qq.com>
2016-10-26 15:29:06 +0800

    stream

---
+++ homework/1026/yuyao/Fstream.java
@@ -0,0 +1,15 @@
+package ¡Ÿ ±;
+
+import java.util.Scanner;
+
+public class Fstream {
+	public static void main(String[] args) {
+		
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		while (scanner.hasNext()){
+			String line = scanner.nextLine();
+			System.out.println(line);
+		}
+	}
+}
