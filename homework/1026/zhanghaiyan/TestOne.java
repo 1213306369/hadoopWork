93ee0c0686fe580c6ccdff856e0a1c0212ebada6
WilliamKZhy <894494132@qq.com>
2016-10-26 00:03:39 -0700

    testone

---
+++ homework/1026/zhanghaiyan/TestOne.java
@@ -0,0 +1,15 @@
+package Onetest;
+
+import java.util.Scanner;
+
+public class TestOne {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext())
+		{
+			String line = sc.next();
+			System.out.println(line);
+		}
+		
+	}
+}
