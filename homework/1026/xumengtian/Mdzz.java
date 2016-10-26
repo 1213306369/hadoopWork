9a33d9f5087025ba50506a26d7e14b1349e2193d
xumengtian <779152076@qq.com>
2016-10-26 15:12:32 +0800

    add a file

---
+++ homework/1026/xumengtian/Mdzz.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Mdzz {
+
+	public static void main(String[] args) {
+		Scanner sc;
+		sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			String line =sc.nextLine();
+			System.out.println(line);
+		}
+
+	}
+
+}
