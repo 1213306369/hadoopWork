7678c9c001aca4889df58b063657f03904ef0961
1213306369 <1213306369@qq.com>
2016-10-27 19:05:02 +0800

    Update Zhengze.java

--- homework/1026/huangxiaonan/Zhengze.java
+++ homework/1026/huangxiaonan/Zhengze.java
@@ -6,18 +6,22 @@ import java.util.regex.Pattern;
 
 public class Zhengze {
 	public static void main(String[] args) {
+		int count = 0;
 		Scanner sc = new Scanner(System.in);
-		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[[^ ]* [^ ]*\\] \"[^ ]+ ([^ ]+).*";
+		
+		//只匹配出IP、时间和访问的URL
+		//String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* (\\[[^ ]* [^ ]*\\]) \"[^ ]+ ([^ ]+).*";
 		//String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* [^ ]* [^ ]* [^ ]+ ([^ ]+).*";
+		//String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* (\\[[^ ]* [^ ]*\\]) \"([^ ]+) ([^ ]+) ([^ ]+)\" (\\d+) (\\d+) \"([^ ]+)\" \"([^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+)\"";
+		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* (\\[[^ ]* [^ ]*\\]) \"([^ ]+) ([^ ]+) ([^ ]+)\" (\\d+) (\\d+) \"([^ ]+)\" \"(.*)\"";
 		Pattern r = Pattern.compile(pattern);
 		while(sc.hasNext()){
 			String line = sc.nextLine();
 			Matcher m = r.matcher(line);
 			if(m.find()){
 				//System.out.printf("%s\t%s\n",m.group(1),m.group(2));
-				System.out.println(m.group(1)+"\t"+m.group(2));
+				System.out.println(m.group(1)+"\t"+m.group(2)+"\t"+m.group(3)+"\t"+m.group(4)+"\t"+m.group(5)+"\t"+m.group(6)+"\t"+m.group(7)+"\t"+m.group(8)+"\t"+m.group(9)+"\t");
 			}
-		}
+		}	
 	}
-
 }
